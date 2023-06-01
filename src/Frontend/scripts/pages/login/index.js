076828e1c6f07c813d611ea066348e6d19d6a2e5
Eduardo Barreto <eduardopontobarreto@gmail.com>
2023-06-01 15:22:34 -0300

    feat: :sparkles: login integration

---
+++ src/Frontend/scripts/pages/login/index.js
@@ -0,0 +1,42 @@
+const LOGIN_URL = "http://localhost:3000/login";
+
+document.addEventListener("DOMContentLoaded", function () {
+    var form = document.querySelector(".form-login");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        var email = form.email.value;
+        var password = form.password.value;
+
+        if (email === "") {
+            alert("Preencha o campo de e-mail");
+            return;
+        }
+
+        if (password === "") {
+            alert("Preencha o campo de senha");
+            return;
+        }
+
+        fetch(LOGIN_URL, {
+            method: "POST",
+            body: JSON.stringify({
+                email: email,
+                password: password
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(function (response) {
+            if (response.status === 200) {
+                alert("Login realizado com sucesso!");
+                window.location.href = "../../../pages/home/index.html";
+            } else {
+                alert("Ocorreu um erro no login");
+            }
+        }).catch(function (error) {
+            alert("Ocorreu um erro no login");
+        });
+    })
+});
