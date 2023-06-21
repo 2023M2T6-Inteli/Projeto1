7f7d04909241738acbca90fd0e233a0b186d3dd5
Eduardo Barreto <eduardopontobarreto@gmail.com>
2023-06-21 08:57:32 -0300

    fix: :bug: fix pages router route
    port is now 1234

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -14,7 +14,7 @@ const classesRouter = require('./routers/class-router');
 const teachersRouter = require('./routers/teacher-router');
 
 const HOST = '127.0.0.1';
-const PORT = 3000;
+const PORT = 1234;
 
 app.use(express.json());
 app.use(cors());
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../Frontend/pages/home/index.html'));
 });
 
-app.use('/api/', pagesRouter);
+app.use('/', pagesRouter);
 app.use('/api/grades', gradesRouter);
 app.use('/api/activities', activitiesRouter);
 app.use('/api/classes', classesRouter);
