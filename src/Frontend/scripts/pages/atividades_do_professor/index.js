56b884d2726e5f9789dac0f2191a0b34949c4a17
Guilherme Borges <guilhermepardi@gmail.com>
2023-06-21 08:49:47 -0300

    feat: integration pages registro_de_atividades e atividades_do_professor

--- src/Frontend/scripts/pages/atividades_do_professor/index.js
+++ src/Frontend/scripts/pages/atividades_do_professor/index.js
@@ -1,71 +1,122 @@
-async function getContents(subject, hits) {
-  // fetch na URL de busca do Algolia com query params
-  const response = await fetch(`https://6I7NDWQ9YU-dsn.algolia.net/1/indexes/conteudo-pane-teste?query=${subject}&hitsPerPage=${hits}`, {
-    method: 'GET',
-    headers: {
-      // Headers necessários para autenticação no Algolia
-      'X-Algolia-Application-Id': '6I7NDWQ9YU',
-      'X-Algolia-API-Key': '459b8ac86fdd4dc47c31095c2dd12e2f'
-    }
-  });
-  // Transforma a resposta em JSON
-  const json = await response.json();
-  // Retorna o array de hits
-  return json.hits;
+function capitalizeFirstLetter(str) {
+  if (str.length === 0) {
+    return str;
+  }
+
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  let teacherId = localStorage.getItem('teacherID')
+async function getContents(subject, hits) {
+  try {
+    const response = await fetch(
+      `https://6I7NDWQ9YU-dsn.algolia.net/1/indexes/conteudo-pane-teste?query=${subject}&hitsPerPage=${hits}`,
+      {
+        method: 'GET',
+        headers: {
+          'X-Algolia-Application-Id': '6I7NDWQ9YU',
+          'X-Algolia-API-Key': '459b8ac86fdd4dc47c31095c2dd12e2f'
+        }
+      }
+    );
 
-  const defaultUrl = `http://127.0.0.1:3000/teachers/${teacherId}/activities`
+    const json = await response.json();
+    return json.hits;
+  } catch (error) {
+    console.error('Ocorreu um erro na requisição:', error);
+    throw error;
+  }
+}
 
-  fetch(defaultUrl, {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json"
-    }
-  }).then(function (response) {  
-    response.json().then((data) => {
-      data.map(activity => {
-        getContents(activity.skill_id, 1).then(content => {
-          const discipline = content[0].disciplina[0].toLowerCase();
-          const container = document.getElementsByClassName('disciplines-container')[0];
-
-          let divElement = container.querySelector(`.${discipline}`);
-
-          if (!divElement) {
-            divElement = document.createElement('div');
-            divElement.classList.add('discipline-container', discipline);
-
-            const heading = document.createElement('h2')
-            heading.textContent = discipline
-
-            const ulElement = document.createElement('ul');
-            const liElement = document.createElement('li');
-            const paragraphElement = document.createElement('p');
-            paragraphElement.textContent = activity.activity_name;
-
-            liElement.appendChild(paragraphElement);
-            ulElement.appendChild(liElement);
-            divElement.appendChild(heading);
-            divElement.appendChild(ulElement);
-
-            container.appendChild(divElement);
-          } else {
-            const ulElement = divElement.querySelector('ul');
-            const liElement = document.createElement('li');
-            const paragraphElement = document.createElement('p');
-            paragraphElement.textContent = activity.activity_name;
-
-            liElement.appendChild(paragraphElement);
-            ulElement.appendChild(liElement);
-          }
-        })
-      })
-    });
-    
+document.addEventListener('DOMContentLoaded', async () => {
+  const teacherId = localStorage.getItem('teacherID');
   
-  }).catch(function (error) {
-    alert(`Ocorreu um erro: ${error}`);
-  });
-})
+  if (!teacherId) {
+    console.error('ID do professor não encontrado no localStorage.');
+    return;
+  }
+  
+  const defaultUrl = `http://127.0.0.1:3000/api/teachers/${teacherId}/activities`;
+
+  try {
+    const response = await fetch(defaultUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status} - ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    const container = document.querySelector('.disciplines-container');
+    const selectElement = document.querySelector('.filters-container-select');
+
+    for (const activity of data) {
+      try {
+        const content = await getContents(activity.skill_id, 1);
+        const discipline = content[0]?.disciplina?.[0]?.toLowerCase();
+
+        if (!discipline) {
+          console.warn(`Disciplina não encontrada para a atividade: ${activity.activity_name}`);
+          continue;
+        }
+
+        let divElement = container.querySelector(`.${discipline}`);
+
+        if (!divElement) {
+          divElement = document.createElement('div');
+          divElement.classList.add('discipline-container', discipline);
+
+          const heading = document.createElement('h2');
+          heading.textContent = capitalizeFirstLetter(discipline);
+
+          const ulElement = document.createElement('ul');
+          const liElement = document.createElement('li');
+          const paragraphElement = document.createElement('p');
+          paragraphElement.textContent = activity.activity_name;
+
+          liElement.appendChild(paragraphElement);
+          ulElement.appendChild(liElement);
+          divElement.appendChild(heading);
+          divElement.appendChild(ulElement);
+
+          container.appendChild(divElement);
+
+          const option = document.createElement('option');
+          option.textContent = discipline;
+          selectElement.appendChild(option);
+        } else {
+          const ulElement = divElement.querySelector('ul');
+          const liElement = document.createElement('li');
+          const paragraphElement = document.createElement('p');
+          paragraphElement.textContent = activity.activity_name;
+
+          liElement.appendChild(paragraphElement);
+          ulElement.appendChild(liElement);
+        }
+      } catch (error) {
+        console.error(`Ocorreu um erro ao processar a atividade: ${activity.activity_name}`, error);
+      }
+    }
+
+    selectElement.addEventListener('change', function () {
+      const selectedOption = selectElement.value.toLowerCase();
+
+      const divElements = document.querySelectorAll('.discipline-container');
+
+      divElements.forEach(divElement => {
+        if (divElement.classList.contains(selectedOption)) {
+          divElement.style.display = 'block';
+        } else {
+          divElement.style.display = 'none';
+        }
+      });
+    });
+  } catch (error) {
+    console.error(`Ocorreu um erro na requisição: ${error}`);
+    alert(`Ocorreu um erro na requisição: ${error}`);
+  }
+});
---
+++ src/Frontend/scripts/pages/registro_de_atividade/index.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.content-container');
+
+  form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const activityName = form.querySelector('input[type="text"]').value;
+    const activityDescription = form.querySelector('textarea').value;
+    const skillId = form.querySelector('select').value;
+    const teacherId = localStorage.getItem('teacherID');
+
+    const postData = {
+      activity_name: activityName,
+      activity_description: activityDescription,
+      skill_id: skillId,
+      teacher_id: teacherId
+    };
+
+    try {
+      const response = await fetch('http://127.0.0.1:3000/api/activities', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+      });
+
+      if (response.status = 201) {
+        alert('Atividade criada com sucesso!');
+        form.reset();
+      } else {
+        alert('Ocorreu um erro ao criar a atividade.');
+      }
+    } catch (error) {
+      alert(`Ocorreu um erro: ${error}`);
+    }
+  });
+});
